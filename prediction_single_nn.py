d61b47cf8c84d1416bf894dd9832fd7be9d4e809
Shivam <51316625+Shivam101s@users.noreply.github.com>
2024-12-11 04:37:17 -0500

    Update prediction_single_nn.py

--- prediction_single_nn.py
+++ prediction_single_nn.py
@@ -15,6 +15,8 @@ if not os.path.exists('plots2'):
 
 # Step 1: Load and preprocess the dataset
 df = pd.read_csv('processed_my.csv')
+df = df.rename(columns={"Min_Pressure": "Max_Sustained_Wind"})
+
 
 # Preprocessing the dataset
 def process_latitude(lat):
@@ -29,7 +31,7 @@ df['Longitude_EW'] = df['Longitude_EW'].apply(process_longitude)
 df['Storm_Intensity'] = df['Storm_Intensity'].astype('category')
 
 # Step 2: Define features and target variable
-X = df[['Latitude_NS', 'Longitude_EW', 'Min_Pressure']]
+X = df[['Latitude_NS', 'Longitude_EW', 'Max_Sustained_Wind']]
 y = df['Storm_Intensity']
 y = y.cat.codes
 
